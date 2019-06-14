f0b981e429a62e6a4b98b3b5f3ccb4e4784f1eed
Tori <victoria.fafrak@gmail.com>
2019-06-14 18:39:31 -0400

    backend work on routes and controllers

---
+++ controllers/childController.js
@@ -0,0 +1,49 @@
+const db = require('../models')
+module.exports = {
+    findAll: function (req, res) {
+        var query = {};
+        if (req.query.user_id) {
+            query.UserId = req.query.user_id;
+        }
+        db.Child.findAll({
+            where: query,
+            include: [db.User]
+        }).then(function (dbChild) {
+            res.json(dbChild);
+        });
+    },
+    findOne: function (req, res) {
+        db.Child.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.User]
+        }).then(function (dbChild) {
+            res.json(dbChild);
+        });
+    },
+    delete: function (req, res) {
+        db.Child.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbChild) {
+            res.json(dbChild);
+        });
+    },
+    create: function (req, res) {
+        db.Child.create(req.body).then(function (dbChild) {
+            res.json(dbChild);
+        });
+    },
+    update: function (req, res) {
+        db.Child.update(
+            req.body, {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function (dbChild) {
+                res.json(dbChild);
+            });
+    }
+}
--- models/actList.js
+++ models/actList.js
@@ -9,20 +9,20 @@ module.exports = function (sequelize, DataTypes) {
 
     // Associating ActList with multiple actList
     // When ActList is deleted, deleted any associated actList
-    ActList.associate = function (models) {
-        ActList.hasMany(models.Activities, {
-            onDelete: "cascade"
-        });
-    };
+    // ActList.associate = function (models) {
+    //     ActList.hasMany(models.Activities, {
+    //         onDelete: "cascade"
+    //     });
+    // };
 
     // ActList must belong to a Activity
     // a actlist must have Activity to be created
-    ActList.associate = function (models) {
-        ActList.belongsTo(models.Activities, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    }
+    // ActList.associate = function (models) {
+    //     ActList.belongsTo(models.Activities, {
+    //         foreignKey: {
+    //             allowNull: false
+    //         }
+    //     });
+    // }
     return ActList;
 }
