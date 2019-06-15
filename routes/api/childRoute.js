59cb9fa36d5bc61fb58f6d1a5620364cb9dbc4b3
Nitza <rondon.nitza@gmail.com>
2019-06-15 12:24:01 -0400

    Updated child and user route files and updated routes code lines in server file

--- routes/api/childRoute.js
+++ routes/api/childRoute.js
@@ -1,13 +1,13 @@
 var childRouter = require("express").Router();
-var childController = require("../controllers/childController");
+var childController = require("../../controllers/childController");
 
 childRouter.route("/child")
   .get(childController.findAll)
   .post(childController.create)
   .put(childController.update);
 
-childRouter.route("/child/:id")
-  .get(childController.findOne)
-  .delete(childController.delete);
+// childRouter.route("/child/:id")
+//   .get(childController.findOne)
+//   .delete(childController.delete);
 
 module.exports = childRouter;
