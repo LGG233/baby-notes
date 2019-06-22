08b650248354b7be1e2399ede424e5c12ef7715f
Fred Astaire <lancebc@thegodardgroup.com>
2019-06-22 12:39:16 -0400

    show longin / register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
@@ -14,7 +14,7 @@ import Home from './pages/Home';
 import SignIn from "./components/SignIn/SignIn";
 
 class App extends Component {
-  
+
   render() {
     return (
       <Router>
@@ -25,26 +25,26 @@ class App extends Component {
           <br></br>
           <br></br>
           <br></br>
-      <div className="loginSwitch">
-        <div className="container ">
+          <div className="loginSwitch">
+            <div className="container ">
 
-        <NavLink to="/register" activeClassName="loginSwitcher_item-Active"  className="pageSwitcher_item"> Register</NavLink>
-        <NavLink exact to="/signIn" activeClassName="loginSwitcher_item-Active" className="pageSwitcher_item"> Log In</NavLink>
-      </div>
-      </div>
+              <NavLink exact to="/register" activeClassName="loginSwitcher_item-Active" className="pageSwitcher_item"> Register</NavLink>
+              <NavLink exact to="/signin" activeClassName="loginSwitcher_item-Active" className="pageSwitcher_item"> Log In</NavLink>
+            </div>
+          </div>
 
 
-      <br></br>
+          <br></br>
           <Switch>
             <Route exact path="/" component={Landing} />
-            <div className= "container">
-            <Route exact path="/register" component={RegisterForm} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/signIn" compenent={SignIn} />
-            <Route exact path="/activity" component={Activity} />
-            {/* <Route exact path="/child" component={Child} />
+            <div className="container">
+              <Route exact path="/register" component={RegisterForm} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/signin" component={SignIn} />
+              <Route exact path="/activity" component={Activity} />
+              {/* <Route exact path="/child" component={Child} />
             <Route exact path="/journal" component={Journal} /> */}
-            <Route component={NotFound} />
+              <Route component={NotFound} />
             </div>
           </Switch>
         </div>
