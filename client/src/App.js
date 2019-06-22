92b2ff7ca9c03910aabee0b132b49733ec9a92ce
glipps <glipps@kent.edu>
2019-06-22 10:47:22 -0400

    visible signin form on journal route

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import RegisterForm from './components/RegisterForm/RegisterForm';
+import RegisterForm from './components/RegisterForm/RegisterForm';
 import Activity from './pages/Activity';
 import Landing from './pages/Landing';
 import Journal from './pages/Journal';
 import Child from './pages/Child';
 import NotFound from './pages/NotFound';
 // import Home from './pages/Home';
-// import SignIn from './components/SignIn/SignIn';
+import SignIn from './components/SignIn/SignIn';
 
 
 function App() {
@@ -28,11 +28,11 @@ function App() {
         </div>
         <Switch>
           <Route exact path="/" component={Landing} />
-          {/* <Route exact path="/home" component={RegisterForm} /> */}
-          {/* <Route exact path="/" component={SignIn} /> */}
+          <Route exact path="/home" component={RegisterForm} />
+          <Route exact path="/journal" component={SignIn} />
           <Route exact path="/activity" component={Activity} />
           <Route exact path="/child" component={Child} />
-          <Route exact path="/journal" component={Journal} />
+          {/* <Route exact path="/journal" component={Journal} /> */}
           <Route component={NotFound} />
         </Switch>
       </div>
