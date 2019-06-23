5d32560601eadb47036ee51a2bed4fe64be33924
Facundo Rodriguez <facurodri91@gmail.com>
2019-06-23 00:33:57 -0400

    Made some more changes, fixed the footer, still working on things

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,17 +5,15 @@ const Footer = () => {
     return (
         <div className="footer">
             <footer className="footer is-primary">
-                <div className="container">
-                    <div className="colums">
-                        <div className="column">
-                            <p>&copy; 2019 The Gang</p>
-                        </div>
-                        <div className="column-has-text-right">
-                            <a className="icon" href="https://github.com/"><i className="#"></i></a>
-                            <a className="icon" href="https://www.heroku.com/home"><i className="#"></i></a>
-                        </div>
-                    </div>
-                </div>
+                {/* <div className="container"> */}
+                    {/* <div className="colums"> */}
+                       <p>&copy; 2019 The Gang</p>
+                        {/* <div className="column-has-text-right"> */}
+                            <a className="icon" href="https://github.com/"><i className="#">Github</i></a>
+                            <a className="icon" href="https://www.heroku.com/home"><i className="#"> Heroku</i></a>
+                         {/* </div> */}
+                   {/* </div> */}
+                {/* </div> */}
             </footer>
         </div>
     );
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,32 @@
 import React from "react";
+import {
+    BrowserRouter as 
+    NavLink
+  } from "react-router-dom";
 
 function Landing() {
     return (
         <div>
-            <h1>LANDING PAGE</h1>
+             <div className="loginSwitch">
+            <NavLink
+              exact
+              to="/register"
+              activeClassName="loginSwitcher_item-Active"
+              className="pageSwitcher_item"
+            >
+              {" "}
+              Register
+            </NavLink>
+            <NavLink
+              exact
+              to="/signin"
+              activeClassName="loginSwitcher_item-Active"
+              className="pageSwitcher_item"
+            >
+              {" "}
+              Log In
+            </NavLink>
+          </div>
         </div>
     );
 }
