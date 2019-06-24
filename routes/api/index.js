429f3565059ed37304cbec6ae3297671357ce28b
Nitza <rondon.nitza@gmail.com>
2019-06-23 20:06:29 -0400

    Updated controllers and routes index files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const router = require("express").Router();
-// const gameRoutes = require("./games");
-const userRoutes = require("./userRoute");
-const childRoutes = require("./childRoute");
+// const path = require("path");
+// const router = require("express").Router();
+// // const gameRoutes = require("./games");
+// const userRoutes = require("./userRoute");
+// const childRoutes = require("./childRoute");
 
-// router.use("/games", gameRoutes);
-router.use("/api", userRoutes);
-router.use("/api", childRoutes);
+// // router.use("/games", gameRoutes);
+// router.use("/api", userRoutes);
+// router.use("/api", childRoutes);
 
-// For anything else, render the html page
-router.use(function (req, res) {
-    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
+// // For anything else, render the html page
+// router.use(function (req, res) {
+//     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+// });
 
-module.exports = router;
+// module.exports = router;
 
 //CODE FROM CONFESSIONS INDEX:
 // const path = require('path');
