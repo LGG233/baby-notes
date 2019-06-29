447fc93412b0342c616be051442453fc9a7fb8c5
Gregory Setnik <gsetnik18@gmail.com>
2019-06-29 09:33:37 -0400

    pulling to merge

--- client/src/components/FeedingTable/FeedingTable.js
+++ client/src/components/FeedingTable/FeedingTable.js
@@ -3,9 +3,9 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 
 const data = [{
+    date: '05/22/2019',
     start: '10:00 PM',
     end: '4:00 PM',
-    date: '05/22/2019',
     observations: <p>Jimmy ate a lot more today</p>
 }]
 
--- client/src/components/SleepTable/SleepTable.js
+++ client/src/components/SleepTable/SleepTable.js
@@ -3,9 +3,9 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 
 const data = [{
+    date: '05/22/1992',
     start: '10:00 PM',
     end: '4:00 PM',
-    date: '05/22/2019',
     observations: <p>The day started with an early morning walk to the park.</p>
 }]
 
