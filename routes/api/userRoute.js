5472ab388926fb48041e733f62655c738fae5418
Nitza <rondon.nitza@gmail.com>
2019-06-30 21:25:02 -0400

    Added some tweaks for deserialize serialize and some new packages.

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -11,16 +11,21 @@
 // *****************************************************
 var db = require("../../models");
 var passport = require("../../config/passport");
+var auth = require("../../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
 
-  app.get("/user/login", passport.authenticate("local"), (req, res) => {
+  app.post("/user/login", passport.authenticate("local"), (req, res) => {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
 
     // db.User.findOne({ where : { id: req.user.id}})
     //   .then(dbData => console.log(dbData))
+    
+    
+    console.log("hello");
+			
 
     db.User.findOne({
         where: {
@@ -78,7 +83,7 @@ module.exports = function (app) {
 
   // Route for logging user out
   app.get("/logout", (req, res) => {
-   
+    req.logout();
     res.redirect("/");
   });
 
