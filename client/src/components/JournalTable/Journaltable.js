57eadd2ace17fc35176094e350595ecca4c975f3
Fred Astaire <lancebc@thegodardgroup.com>
2019-07-06 20:25:48 -0400

    added delete button to Journal Table

--- client/src/components/JournalTable/Journaltable.js
+++ client/src/components/JournalTable/Journaltable.js
@@ -4,38 +4,55 @@ import 'react-table/react-table.css';
 import "./JournalTable.css";
 import moment from 'moment';
 
-const columns = [{
-    Header: '',
-    id: 'edit',
-    accessor: 'id',
-    Cell: ({ value }) => (<button className="btn btn-success" onClick={(() => console.log("THis IS THE EDIT BUTTON and "))} >Edit</button>),
-    width: 70
-},
-{
-    Header: 'Date',
-    id: 'date',
-    headerStyle: { textAlign: 'left' },
-    width: 93,
-    accessor: d => {
-        return moment(d.date)
-            .local()
-            .format("MMMM D, YYYY")
+const columns = [
+    {
+        Header: '',
+        id: 'edit',
+        accessor: 'id',
+        Cell: row => (
+            <div>
+                <button className="btn btn-success" onClick={(() => console.log("THis IS THE DELETE BUTTON and "))} >Del</button>
+            </div>
+        ),
+        width: 60
+    },
+    {
+        Header: 'Date',
+        id: 'date',
+        headerStyle: { textAlign: 'left' },
+        width: 93,
+        accessor: d => {
+            return moment(d.date)
+                .local()
+                .format("MMMM D, YYYY")
+        }
+    },
+    {
+        Header: 'Title',
+        accessor: 'title',
+        headerStyle: { textAlign: 'left' },
+        // style: { 'whiteSpace': 'unset' },
+        width: 100
+    },
+    {
+        Header: 'Notes',
+        accessor: 'description',
+        headerStyle: { textAlign: 'left' },
+        // style: { 'whiteSpace': 'unset' },
+        width: 250
+    },
+    {
+        Header: '',
+        id: 'edit',
+        accessor: 'id',
+        Cell: row => (
+            <div>
+                <button className="btn btn-success" onClick={(() => console.log("THis IS THE EDIT BUTTON and "))} >Edit</button>
+            </div>
+        ),
+        width: 60
     }
-},
-{
-    Header: 'Title',
-    accessor: 'title',
-    headerStyle: { textAlign: 'left' },
-    // style: { 'whiteSpace': 'unset' },
-    width: 100
-},
-{
-    Header: 'Notes',
-    accessor: 'description',
-    headerStyle: { textAlign: 'left' },
-    // style: { 'whiteSpace': 'unset' },
-    width: 250
-}]
+]
 
 class JournalTable extends Component {
     constructor(props) {
