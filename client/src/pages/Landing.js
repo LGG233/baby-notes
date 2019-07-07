c296dc7b016f5f60af0cd683b39b7143c1ee56a6
Tori <victoria.fafrak@gmail.com>
2019-07-07 13:18:58 -0400

    removing unnecessary files on backend

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -10,7 +10,7 @@ const Landing = function () {
     <div className="container-fluid">
       <div className="col-md-12 loginSwitch">
         <h1>Welcome to Baby Notes</h1>
-        <p>Talk about what app does</p>
+        <p>Babies bring long days and short years for new parents! Baby Notes lets you keep track of her feeding, sleeping, and diaper changes, and lets you easily monitor developments in real time. What's more, there's a personal journal where you can record milestone moments from first smiles and giggles to first steps! Register for an account or sign in now!</p>
       </div>
     </div>
 
