bc7c2495aab4b953131967b8f809939ec35ed439
Tori <victoria.fafrak@gmail.com>
2019-07-09 19:38:28 -0400

    attempt

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./api");
+// const apiRoutes = require("./api");
 
+const actRoute = require("./api/activitiesRoute");
+const userRoute = require("./api/userRoute");
+const childRoute = require("./api/childRoute");
+
+//API routes
+router.use("/", actRoute);
+router.use("/", userRoute);
+router.use("/", childRoute);
 // API Routes
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
