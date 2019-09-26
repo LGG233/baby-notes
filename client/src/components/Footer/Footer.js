8bc9bbb850cd527fd6cbd829a2307fd9d1363e69
Fred Astaire <lancebc@thegodardgroup.com>
2019-09-26 17:28:28 -0400

    Change team name in footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,17 +4,17 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-     
-        <p>&copy; 2019 Group: 404 Not Found</p>
-        
-          <a className="" href="https://github.com/LGG233/baby-notes">
-            <i className="#">Github</i>
-          </a>
-          <a className="" href="https://sleepy-chamber-22147.herokuapp.com/">
-            <i className="#"> Heroku</i>
-          </a>
-        
-      
+
+      <p>&copy; 2019: Team 404</p>
+
+      <a className="" href="https://github.com/LGG233/baby-notes">
+        <i className="#">Github</i>
+      </a>
+      <a className="" href="https://sleepy-chamber-22147.herokuapp.com/">
+        <i className="#"> Heroku</i>
+      </a>
+
+
     </div>
   );
 };
